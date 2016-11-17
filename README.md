2bd210e3e86fc2ae5aab0c13c6cadc79b8721028
ageyev <ageyev@gmail.com>
2016-11-17 17:53:35 +0200

    add RIPEMD-160

---
+++ README.md
@@ -0,0 +1,7 @@
+
+# Java Crypto Examples 
+
+Using Cryptography on Java: code snippets, examples, exercises 
+
+[RIPEMD-160](src/main/java/Ripemd160.java)
+
---
+++ src/main/java/Ripemd160.java
@@ -0,0 +1,29 @@
+import org.bouncycastle.crypto.digests.RIPEMD160Digest;
+import org.bouncycastle.util.encoders.Hex;
+
+/*
+See: https://rosettacode.org/wiki/RIPEMD-160#Java
+*/
+public class Ripemd160 {
+
+    static String messageText = "this is a message";
+
+    public static void main(String[] argv) throws Exception {
+
+        byte[] r = messageText.getBytes("UTF-8");
+
+        RIPEMD160Digest d = new RIPEMD160Digest();
+        d.update(r, 0, r.length);
+
+        byte[] o = new byte[d.getDigestSize()];
+        d.doFinal(o, 0);
+
+        // Hex.encode(o, System.out);
+        // System.out.println(); // 45147c708948188cead54a10b95899a36f47dc9c
+
+        String digestStr = Hex.toHexString(o);
+        System.out.println(digestStr); // 45147c708948188cead54a10b95899a36f47dc9c
+
+    }
+
+}
